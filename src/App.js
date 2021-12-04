f9a27221fffde2ab3bc01494993f644757cca4ac
koolmilk <sbosse1409@googlemail.com>
2021-12-04 16:10:01 +0100

    Changed some Text & deleted React logo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Just a sample app with React
         </p>
         <a
           className="App-link"
@@ -15,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn More
         </a>
       </header>
     </div>
